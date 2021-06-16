622e05e8e63111c008a0d153fcea29e5727c5c66
Xiaoyi Chen <chirping78@hotmail.com>
2021-06-16 10:20:15 +0800

    The Discovery book --> 5.4. led and delay

---
+++ examples/led_roulette.rs
@@ -0,0 +1,43 @@
+#![no_std]
+#![no_main]
+
+// pick a panicking behavior
+use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+// use panic_abort as _; // requires nightly
+// use panic_itm as _; // logs messages over ITM; requires ITM support
+// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+use cortex_m_rt::entry;
+
+use stm32f4xx_hal as hal;
+
+use hal::{prelude::*, stm32};
+
+#[entry]
+fn main() -> ! {
+    let (cp, dp) = (
+        cortex_m::peripheral::Peripherals::take().unwrap(),
+        stm32::Peripherals::take().unwrap()
+    );
+
+    let rcc = dp.RCC.constrain();
+    let clocks = rcc.cfgr.sysclk(168.mhz()).freeze();
+    let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
+
+    let gpiod = dp.GPIOD.split();
+    let mut led = (
+        gpiod.pd12.into_push_pull_output(),
+        gpiod.pd13.into_push_pull_output(),
+        gpiod.pd14.into_push_pull_output(),
+        gpiod.pd15.into_push_pull_output(),
+    );
+
+    let half_period = 500_u16;
+    loop {
+        led.0.set_high().unwrap();
+        delay.delay_ms(half_period);
+
+        led.0.set_low().unwrap();
+        delay.delay_ms(half_period);
+    }
+}
